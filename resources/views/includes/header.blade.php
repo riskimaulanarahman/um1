3eec45d4cddd1102b79e5b027c24986883cbdfc5
Riski Maulana Rahman <riskimaulanarahman@Riskis-MacBook-Air.local>
2021-06-01 18:51:43 +0800

    add edit password

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -145,7 +145,9 @@
 				<span class="d-none d-md-inline">{{ Auth::user()->name }}</span> <b class="caret"></b>
 			</a>
 			<div class="dropdown-menu dropdown-menu-right">
+				@if(Auth::user()->role == 'warga')
 				<a href="javascript:;" class="dropdown-item" data-toggle="modal" data-target="#modal-edit-password">Edit Password</a>
+				@endif
 				{{-- <a href="javascript:;" class="dropdown-item"><span class="badge badge-danger pull-right">2</span> Inbox</a>
 				<a href="javascript:;" class="dropdown-item">Calendar</a>
 				<a href="javascript:;" class="dropdown-item">Setting</a> --}}
